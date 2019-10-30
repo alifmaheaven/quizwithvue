835c2a315532dc1c5330552a0d3768d6d7d533dc
Mohammad Alif Maheaven Hidayatullah <alifmaheaven2001@gmail.com>
2019-10-30 16:30:29 +0700

    finish make exercise

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled> <b>Active Fancy Quize App</b> </b-nav-item>
+            <b-nav-item disabled>Counter : {{numCorrect}}/{{numTotal}}</b-nav-item>
+            <!-- <b-nav-item>Another Link</b-nav-item>
+            <b-nav-item disabled>Disabled</b-nav-item> -->
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 
+// library
+import BootstrapVue from 'bootstrap-vue'
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// import
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
